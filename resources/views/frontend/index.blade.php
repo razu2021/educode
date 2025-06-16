677caa70db8f41b41f94e80bdbc57bfb9a57eb02
Md Razu Hossain Raj <mdrazuhossainraj@gmail.com>
2025-06-17 01:45:34 +0600

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,34 +9,33 @@
 
 
 <!--   banner hero section  -->
-<div class="container col-xl-10 col-xxl-8 px-4 py-5">
-    <div class="row align-items-center g-lg-5 py-5">
-      <div class="col-lg-7 text-center text-lg-start">
-        <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">VWelcome to Educode !!!</h1>
-        <p class="col-lg-10 fs-4">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+<section class="banner1">
+  <div class="container banner1bg">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-8 col-xl-8 col-xxl-8 ">
+        <div class="banner1_content">
+          <h1>Start Learning From Best <br> <strong> Platform </strong></h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, adipisci culpa quibusdam ab et sapiente fuga a aut! Obcaecati optio quibusdam voluptatibus animi, labore blanditiis rerum quasi cumque fugit amet?</p>
+
+        </div>
       </div>
-      <div class="col-md-10 mx-auto col-lg-5">
-        <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
-          <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div class="form-floating mb-3">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
-          <div class="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me"> Remember me
-            </label>
-          </div>
-          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
-          <hr class="my-4">
-          <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
-        </form>
+      {{-- col end  --}}
+      <div class="col-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+        <div class="banner1_image">
+          <img src="{{asset('contents/frontend/assets/assetss')}}/images/banner/banner1.png" alt="Banner image" w-25>
+        </div>
       </div>
+      {{-- col end  --}}
+
     </div>
   </div>
+</section>
+
+
+
+
+
+
 
 
   </main>
